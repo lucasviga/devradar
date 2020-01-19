59b870b0048b68156823f60077f08becdb56a49b
Lucas Viga <lucasviga12@gmail.com>
2020-01-18 23:55:55 -0400

    create inputs and css profile page

--- mobileExpo/src/pages/Main.js
+++ mobileExpo/src/pages/Main.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Image, View, Text } from 'react-native';
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity } from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
+import { MaterialIcons } from '@expo/vector-icons';
 
 const styles = StyleSheet.create({
   map: {
@@ -28,9 +29,42 @@ const styles = StyleSheet.create({
   devTechs: {
     marginTop: 5
   },
+  searchForm: {
+    position: 'absolute',
+    bottom: 20,
+    left: 20,
+    right: 20,
+    zIndex: 5,
+    flexDirection: 'row'
+  },
+  searchInput: {
+    flex: 1,
+    height: 50,
+    backgroundColor: '#fff',
+    color: '#333',
+    borderRadius: 25,
+    paddingHorizontal: 20,
+    fontSize: 16,
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      width: 4,
+      height: 4,
+    },
+    elevation: 2,
+  },
+  loadButton: {
+    width: 50,
+    height: 50,
+    backgroundColor: '#7d40e7',
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 15,
+  },
 })
 
-function Main() {
+function Main({ navigation }) {
 
   const [currentRegion, setCurrentRegion] = useState(null);
 
@@ -62,19 +96,37 @@ function Main() {
   }
 
   return (
-    <MapView initialRegion={currentRegion} style={styles.map}>
-      <Marker coordinate={{ latitude: -3.0075431, longitude: -59.9876538 }}>
-        <Image  style={styles.avatar} source={{ uri: 'https://avatars0.githubusercontent.com/u/31857287?s=460&v=4' }} />
+    <>
+      <MapView initialRegion={currentRegion} style={styles.map}>
+        <Marker coordinate={{ latitude: -3.0075431, longitude: -59.9876538 }}>
+          <Image  style={styles.avatar} source={{ uri: 'https://avatars0.githubusercontent.com/u/31857287?s=460&v=4' }} />
 
-        <Callout>
-          <View style={styles.callout}>
-            <Text style={styles.devName}>Lucas Viga</Text>
-            <Text style={styles.devBio}>Bio iodfisaofiasofdsifodsfd</Text>
-            <Text style={styles.devTechs}>React, Node, Js</Text>
-          </View>
-        </Callout>
-      </Marker>
-    </MapView>
+          <Callout onPress={() => {
+            navigation.navigate('Profile', { github_username: 'lucasviga' });
+          }}>
+            <View style={styles.callout}>
+              <Text style={styles.devName}>Lucas Viga</Text>
+              <Text style={styles.devBio}>Bio iodfisaofiasofdsifodsfd</Text>
+              <Text style={styles.devTechs}>React, Node, Js</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+
+      <View style={styles.searchForm}>
+        <TextInput 
+          style={styles.searchInput}
+          placeholder="Buscar devs por techs..."
+          placeholderTextColor="#999"
+          autoCapitalize="words"
+          autoCorrect={false}
+        />
+
+        <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+          <MaterialIcons name="my-location" size={20} color="#FFF" />
+        </TouchableOpacity>
+      </View>
+    </>
   );
 }
 
